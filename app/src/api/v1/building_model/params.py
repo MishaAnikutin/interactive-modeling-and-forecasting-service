eb539cffa4aa2e075d034087404ff7e412779bf6
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-25 02:25:31 +0300

    get arimax params handler

---
+++ app/src/api/v1/building_model/params.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from dishka import FromDishka
+from dishka.integrations.fastapi import inject_sync
+
+from src.core.application.building_model.schemas.arimax import ArimaxParams
+from src.core.application.building_model.use_cases.params import ArimaxParamsUC
+
+params_router = APIRouter(prefix="/params", tags=["Параметры модели"])
+
+
+@params_router.get("/arimax")
+@inject_sync
+def params_arimax(
+    arimax_params_uc: FromDishka[ArimaxParamsUC]
+) -> ArimaxParams:
+    return arimax_params_uc.execute()
