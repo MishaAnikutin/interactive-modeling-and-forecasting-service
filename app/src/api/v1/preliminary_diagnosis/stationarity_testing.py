b0f7ac2b992d3aa60632f8122b9d8b8a8073213d
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-25 18:02:04 +0300

    импорт один добавил в стац тест

--- app/src/api/v1/preliminary_diagnosis/stationarity_testing.py
+++ app/src/api/v1/preliminary_diagnosis/stationarity_testing.py
@@ -3,6 +3,7 @@ from dishka import FromDishka
 from dishka.integrations.fastapi import inject_sync
 
 from src.core.application.preliminary_diagnosis.schemas.stats_test import StatTestRequest, StatTestResult
+from src.core.application.preliminary_diagnosis.use_cases.stats_test import StationaryTestingUC
 
 stationary_testing_router = APIRouter(prefix="/stationary_testing", tags=["Анализ ряда на стационарность"])
 
