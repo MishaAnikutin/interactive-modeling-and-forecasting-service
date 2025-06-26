d171db22864b88a366dec4b6191cce4d5c20578f
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-26 03:56:37 +0300

    поправил апи ручку для стат тестов

--- app/src/api/v1/preliminary_diagnosis/stationarity_testing.py
+++ app/src/api/v1/preliminary_diagnosis/stationarity_testing.py
@@ -3,15 +3,15 @@ from dishka import FromDishka
 from dishka.integrations.fastapi import inject_sync
 
 from src.core.application.preliminary_diagnosis.schemas.stats_test import StatTestRequest, StatTestResult
-from src.core.application.preliminary_diagnosis.use_cases.stats_test import StationaryTestingUC
+from src.core.application.preliminary_diagnosis.use_cases.stats_test import StationarityUC
 
 stationary_testing_router = APIRouter(prefix="/stationary_testing", tags=["Анализ ряда на стационарность"])
 
 
-@stationary_testing_router.get("/dickey_fuller")
+@stationary_testing_router.get("/")
 @inject_sync
-def dickey_fuller(
+def stationary_testing(
     request: StatTestRequest,
-    stationary_testing_uc: FromDishka[StationaryTestingUC]
+    stationary_testing_uc: FromDishka[StationarityUC]
 ) -> StatTestResult:
-    return stationary_testing_uc.dickey_fuller(request)
+    return stationary_testing_uc.execute(request=request)
