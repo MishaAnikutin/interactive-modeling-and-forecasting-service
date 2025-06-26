2f7df1cc80530e662719a5e4ba47279e4e6429f1
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-26 03:38:02 +0300

    обновил схему теста

--- app/src/core/application/preliminary_diagnosis/schemas/stats_test.py
+++ app/src/core/application/preliminary_diagnosis/schemas/stats_test.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.core.domain import Timeseries
 
 
 class StatTestParams(BaseModel):
-    alpha: float
+    test_name: str = Field(title="Название статистического теста", default="dickey_fuller")
+    alpha: float = Field(title="Уровень значимости", ge=0, le=1, default=0.05)
 
 class StatTestRequest(StatTestParams):
     ts: Timeseries
 
 class StatTestResult(StatTestParams):
-    stat_value: float
-    p_value: float
+    stat_value: float = Field(title="Значение статистики теста")
+    p_value: float = Field(
+        title="p-value",
+        description="Вероятность попадания в критическую область при верной нулевой гипотезе"
+    )
