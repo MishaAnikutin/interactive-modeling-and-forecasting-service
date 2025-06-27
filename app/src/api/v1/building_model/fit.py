42a03f65880861bac1587067d274112781a916af
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-28 02:01:45 +0300

    добавил запросы на fit и params для nhits

--- app/src/api/v1/building_model/fit.py
+++ app/src/api/v1/building_model/fit.py
@@ -3,8 +3,9 @@ from dishka import FromDishka
 from dishka.integrations.fastapi import inject_sync
 
 from src.core.application.building_model.schemas.arimax import ArimaxFitRequest, ArimaxFitResult
+from src.core.application.building_model.schemas.nhits import NhitsFitRequest, NhitsFitResult
 from src.core.application.building_model.use_cases.fit_arimax import FitArimaxUC
-
+from src.core.application.building_model.use_cases.fit_nhits import FitNhitsUC
 
 fit_model_router = APIRouter(prefix="/building_model", tags=["Построение модели"])
 
@@ -12,6 +13,16 @@ fit_model_router = APIRouter(prefix="/building_model", tags=["Построени
 @fit_model_router.post("/arimax/fit")
 @inject_sync
 def fit_arimax(
-    request: ArimaxFitRequest, fit_arimax_uc: FromDishka[FitArimaxUC]
+    request: ArimaxFitRequest,
+    fit_arimax_uc: FromDishka[FitArimaxUC]
 ) -> ArimaxFitResult:
     return fit_arimax_uc.execute(request=request)
+
+
+@fit_model_router.get("/nhits/fit")
+@inject_sync
+def fit_nhits(
+    request: NhitsFitRequest,
+    fit_nhits_uc: FromDishka[FitNhitsUC]
+) -> NhitsFitResult:
+    return fit_nhits_uc.execute(request=request)
--- app/src/api/v1/building_model/params.py
+++ app/src/api/v1/building_model/params.py
@@ -3,7 +3,8 @@ from dishka import FromDishka
 from dishka.integrations.fastapi import inject_sync
 
 from src.core.application.building_model.schemas.arimax import ArimaxParams
-from src.core.application.building_model.use_cases.params import ArimaxParamsUC
+from src.core.application.building_model.schemas.nhits import NhitsParams
+from src.core.application.building_model.use_cases.params import ArimaxParamsUC, NhitsParamsUC
 
 params_router = APIRouter(prefix="/params", tags=["Параметры модели"])
 
@@ -14,3 +15,11 @@ def params_arimax(
     arimax_params_uc: FromDishka[ArimaxParamsUC]
 ) -> ArimaxParams:
     return arimax_params_uc.execute()
+
+
+@params_router.get("/nhits")
+@inject_sync
+def params_nhits(
+    nhits_params_uc: FromDishka[NhitsParamsUC]
+) -> NhitsParams:
+    return nhits_params_uc.execute()
