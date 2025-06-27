4357df1af69816fa0f8d8503824dd04c7b55d2bb
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-28 01:17:21 +0300

    сервис

--- app/src/core/application/preliminary_diagnosis/use_cases/stats_test.py
+++ app/src/core/application/preliminary_diagnosis/use_cases/stats_test.py
@@ -1,4 +1,7 @@
+import pandas as pd
+
 from src.core.application.preliminary_diagnosis.schemas.stats_test import StatTestRequest, StatTestResult
+from src.core.domain import Timeseries
 from src.infrastructure.adapters.stationarity_testing.factory import StationarityTestsFactory
 
 
@@ -6,5 +9,17 @@ class StationarityUC:
     def __init__(self, stationarity_tests_factory: StationarityTestsFactory):
         self.stationarity_tests_factory = stationarity_tests_factory
 
-    def execute(self, request: StatTestRequest) -> StatTestResult:
-        raise NotImplementedError
+    @staticmethod
+    def form_series(ts: Timeseries) -> pd.Series:
+        return pd.Series(
+            data=ts.values,
+            index=ts.dates
+        )
+
+    def execute(self, request: StatTestRequest) -> list[StatTestResult]:
+        test_metrics = self.stationarity_tests_factory.apply(
+            stat_tests=request.test_names,
+            ts=self.form_series(request.ts),
+            alpha=request.alpha,
+        )
+        return test_metrics
--- app/src/core/domain/statistical_tests/stationarity_testing_service.py
+++ app/src/core/domain/statistical_tests/stationarity_testing_service.py
@@ -1,20 +1,16 @@
-from abc import ABC
-from typing import Optional, Callable
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import pandas as pd
 
 from src.core.application.preliminary_diagnosis.schemas.stats_test import StatTestResult
-from src.core.domain.statistical_tests.metric import TestMetric
 
 
 class StationarityTestingService(ABC):
-    strategy: Optional[Callable[..., TestMetric]] = None
+    def __init__(self, ts: pd.Series, alpha: float):
+        self.ts = ts
+        self.alpha = alpha
 
-    def apply(self, **kwargs) -> StatTestResult:
-        if self.strategy is not None:
-            stat_metric = type(self).strategy(**kwargs)
-            return StatTestResult(
-                test_name=kwargs.get("test_name"),
-                alpha=kwargs.get("alpha"),
-                stat_value=stat_metric.stat_value,
-                p_value=stat_metric.p_value,
-            )
+    @abstractmethod
+    def apply(self, **kwargs) -> Optional[StatTestResult]:
         raise NotImplementedError
