d15739dfeeddbe566c56b07aa209fa1debc43d19
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-28 01:17:07 +0300

    добавил примеры на схему временного ряда

--- app/src/core/domain/timeseries/timeseries.py
+++ app/src/core/domain/timeseries/timeseries.py
@@ -1,10 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Timeseries(BaseModel):
-    name: str
-    dates: list[datetime]
-    values: list[Optional[float]]
+    name: str = Field(title="Название ряда", default="IPP")
+    dates: list[datetime] = Field(
+        title="Даты",
+        default=[datetime(2023, 1, 1) + timedelta(days=i*30) for i in range(10)]
+    )
+    values: list[Optional[float]] = Field(
+        title="Значения",
+        default=[float(i * 10 + 5) for i in range(10)]
+    )
