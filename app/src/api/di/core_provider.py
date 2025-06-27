62fb194fb4e643c21046de915c2112e2b0536e52
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-28 02:00:59 +0300

    добавил пару провайдеров

--- app/src/api/di/core_provider.py
+++ app/src/api/di/core_provider.py
@@ -1,7 +1,8 @@
 from dishka import Provider, Scope, provide
 
 from src.core.application.building_model.use_cases.fit_arimax import FitArimaxUC
-from src.core.application.building_model.use_cases.params import ArimaxParamsUC
+from src.core.application.building_model.use_cases.fit_nhits import FitNhitsUC
+from src.core.application.building_model.use_cases.params import ArimaxParamsUC, NhitsParamsUC
 from src.core.application.preliminary_diagnosis.use_cases.stats_test import StationarityUC
 
 
@@ -9,5 +10,10 @@ class CoreProvider(Provider):
     scope = Scope.REQUEST
 
     arimax_fit_command = provide(FitArimaxUC, provides=FitArimaxUC)
+    nhits_fit_command = provide(FitNhitsUC, provides=FitNhitsUC)
+
     arimax_params_command = provide(ArimaxParamsUC, provides=ArimaxParamsUC)
+    nhits_params_command = provide(NhitsParamsUC, provides=NhitsParamsUC)
+
     stationarity_test_command = provide(StationarityUC, provides=StationarityUC)
+
