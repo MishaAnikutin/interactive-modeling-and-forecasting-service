f3c6c652957985ff731326e6dc6bace1a16f55f1
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-06-28 01:15:52 +0300

    обновил схему

--- app/src/core/application/preliminary_diagnosis/schemas/stats_test.py
+++ app/src/core/application/preliminary_diagnosis/schemas/stats_test.py
@@ -4,15 +4,21 @@ from src.core.domain import Timeseries
 
 
 class StatTestParams(BaseModel):
-    test_name: str = Field(title="Название статистического теста", default="dickey_fuller")
+    test_name: str = Field(title="Название статистического теста", default="DickeyFuller")
     alpha: float = Field(title="Уровень значимости", ge=0, le=1, default=0.05)
 
-class StatTestRequest(StatTestParams):
+class StatTestRequest(BaseModel):
+    alpha: float = Field(
+        title="Уровень значимости",
+        ge=0, le=1,
+        default=0.05
+    )
+    test_names: list[str] = Field(
+        title="Список названий тестов для проведения.",
+        default=["DickeyFuller"],
+    )
     ts: Timeseries
 
 class StatTestResult(StatTestParams):
     stat_value: float = Field(title="Значение статистики теста")
-    p_value: float = Field(
-        title="p-value",
-        description="Вероятность попадания в критическую область при верной нулевой гипотезе"
-    )
+    p_value: float = Field(title="p-value")
