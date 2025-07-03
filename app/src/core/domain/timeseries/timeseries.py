761e7e2326d5ee0bcb656c185702ebc7c010489c
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-03 23:07:26 +0300

    добавил хорошие примеры в схему

--- app/src/core/domain/timeseries/timeseries.py
+++ app/src/core/domain/timeseries/timeseries.py
@@ -1,16 +1,28 @@
 from datetime import datetime, timedelta
 from typing import Optional
-
+import numpy as np
 from pydantic import BaseModel, Field
 
+n = 120
+
+def gen_dates():
+    dates =[datetime(2023, 1, 1) + timedelta(days=i*30) for i in range(n)]
+    return dates
+
+def gen_values():
+    pageviews = np.random.randint(1000, 5000, size=n)
+    pageviews = pageviews + np.sin(np.arange(n) * 2 * np.pi / 7) * 500  # Добавляем недельную сезонность
+    pageviews = pageviews + np.sin(np.arange(n) * 2 * np.pi / 365) * 1000  # Добавляем годовую сезонность
+    pageviews = pageviews + np.arange(n) * 2  # Добавляем тренд
+    return pageviews.tolist()
 
 class Timeseries(BaseModel):
     name: str = Field(title="Название ряда", default="IPP")
     dates: list[datetime] = Field(
         title="Даты",
-        default=[datetime(2023, 1, 1) + timedelta(days=i*30) for i in range(10)]
+        default=gen_dates()
     )
     values: list[Optional[float]] = Field(
         title="Значения",
-        default=[float(i * 10 + 5) for i in range(10)]
+        default=gen_values()
     )
