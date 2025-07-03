b070bafd769c7bf7a7e1bf343f798f4478519ff9
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-04 00:22:46 +0300

    поправил хранилище чтоб было без багов

--- app/src/infrastructure/adapters/model_storage/storage.py
+++ app/src/infrastructure/adapters/model_storage/storage.py
@@ -4,10 +4,12 @@ from abc import abstractmethod, ABC
 
 class IModelStorage(ABC):
     @abstractmethod
-    def save(self, model_result): ...
+    def save(self, model_result) -> tuple[str, str]:
+        return "заглушка", "заглушка"
 
 
 class MockModelStorage(IModelStorage):
-    def save(self, arimax_fit_result):
+    def save(self, arimax_fit_result) -> tuple[str, str]:
         print("Сохраняем модель!")
         time.sleep(1)
+        return "заглушка", "заглушка"
