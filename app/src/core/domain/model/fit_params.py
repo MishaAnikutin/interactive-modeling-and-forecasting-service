59293312025bec960e4cb00e9acb57271a493dc5
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-09 03:48:29 +0300

    обновил валидацию в параметрах фита

--- app/src/core/domain/model/fit_params.py
+++ app/src/core/domain/model/fit_params.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+
 
 class FitParams(BaseModel):
     val_boundary: datetime = Field(default=datetime(2030, 11, 30))
     train_boundary: datetime = Field(default=datetime(2029, 5, 31))
     forecast_horizon: int = Field(default=12)
+
+    @model_validator(mode='after')
+    def validate_train_val(self):
+        if self.val_boundary < self.train_boundary:
+            raise ValueError("train_boundary должна быть раньше val_boundary")
+        return self
