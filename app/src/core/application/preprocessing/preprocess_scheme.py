1e1e5fdce24ff2a4423738390335e6c95fcf3f2b
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-12 19:21:18 +0300

    добавил схему для предобработки ряда

--- app/src/core/application/preprocessing/preprocess_scheme.py
+++ app/src/core/application/preprocessing/preprocess_scheme.py
@@ -1,5 +1,71 @@
-from pydantic import BaseModel
+from typing import List, Literal, Union, Annotated
+from pydantic import BaseModel, Field, ConfigDict
 
+from src.core.domain import Timeseries
 
-class Preprocess(BaseModel):
-    pass
+class Transformation(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+# 1. Дифференцирование
+class DiffTransformation(Transformation):
+    type: Literal["diff"] = "diff"
+    diff_order: int = Field(..., gt=0)
+
+# 2. Взятие лага
+class LagTransformation(Transformation):
+    type: Literal["lag"] = "lag"
+    order: int = Field(..., gt=0)
+
+# 3. Логарифмирование
+class LogTransformation(Transformation):
+    type: Literal["log"] = "log"
+    a: float = Field(..., gt=0)
+
+# 4. Потенцирование
+class PowTransformation(Transformation):
+    type: Literal["pow"] = "pow"
+    a: float = Field(..., gt=0)
+
+# 5. Нормализация
+class NormalizeTransformation(Transformation):
+    type: Literal["normalize"] = "normalize"
+    method: Literal["standard", "minmax"]
+
+# 6. Экспоненциальное сглаживание (без параметров)
+class ExpSmoothTransformation(Transformation):
+    type: Literal["exp_smooth"] = "exp_smooth"
+
+# 7. Преобразование Бокса-Кокса
+class BoxCoxTransformation(Transformation):
+    type: Literal["boxcox"] = "boxcox"
+    lambda_: float = Field(..., alias="lambda")
+
+# 8. Заполнение пропусков
+class FillMissingTransformation(Transformation):
+    type: Literal["fillna"] = "fillna"
+    method: Literal["last", "backward", "mean", "median", "mode"]
+
+# 9. Скользящее среднее
+class MovingAverageTransformation(Transformation):
+    type: Literal["moving_avg"] = "moving_avg"
+    window: int = Field(..., gt=0)
+
+TransformationUnion = Annotated[
+    Union[
+        DiffTransformation,
+        LagTransformation,
+        LogTransformation,
+        PowTransformation,
+        NormalizeTransformation,
+        ExpSmoothTransformation,
+        BoxCoxTransformation,
+        FillMissingTransformation,
+        MovingAverageTransformation
+    ],
+    Field(discriminator="type")
+]
+
+# Модель запроса
+class PreprocessingRequest(BaseModel):
+    ts: Timeseries
+    transformations: List[TransformationUnion]  # Список преобразований
