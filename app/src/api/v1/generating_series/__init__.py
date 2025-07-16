36bc430576a4b8b69afb512f6aef4c8d28241bd3
mishaanikutin <billyknife@mail.ru>
2025-07-17 00:12:59 +0300

    Выделил контекст создания рядов

---
+++ app/src/api/v1/generating_series/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from .seasonal_decomposition import seasonal_decomposition_router
+
+
+generating_series_router = APIRouter(prefix='/generating_series', tags=['создание рядов'])
+
+generating_series_router.include_router(seasonal_decomposition_router)
---
+++ app/src/api/v1/generating_series/seasonal_decomposition.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from dishka import FromDishka
+from dishka.integrations.fastapi import inject_sync
+
+
+seasonal_decomposition_router = APIRouter(
+    prefix="/seasonaly_decomposition",
+    tags=["Сезонная декомпозиция"]
+)
+
+
