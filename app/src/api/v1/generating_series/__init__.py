8c97a773ddac64634826a0ba31796e4b81bd9e72
mishaanikutin <billyknife@mail.ru>
2025-07-21 10:17:43 +0300

    исправил баг со swagger-ом

--- app/src/api/v1/generating_series/__init__.py
+++ app/src/api/v1/generating_series/__init__.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 from .seasonal_decomposition import seasonal_decomposition_router
 
 
-generating_series_router = APIRouter(prefix='/generating_series', tags=['Создание рядов'])
+generating_series_router = APIRouter(prefix='/generating_series')
 
 generating_series_router.include_router(seasonal_decomposition_router)
