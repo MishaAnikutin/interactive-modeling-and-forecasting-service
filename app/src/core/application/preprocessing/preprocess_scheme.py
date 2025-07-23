1d59788c4af2cbe7db290e9d898d9b80b142bafd
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-23 17:34:35 +0300

    Update preprocess_scheme.py

--- app/src/core/application/preprocessing/preprocess_scheme.py
+++ app/src/core/application/preprocessing/preprocess_scheme.py
@@ -9,12 +9,12 @@ class Transformation(BaseModel):
 # 1. Дифференцирование
 class DiffTransformation(Transformation):
     type: Literal["diff"] = "diff"
-    diff_order: int = Field(..., gt=0)
+    diff_order: int = Field(title="Периоды сдвига для вычисления разности (допустимы отрицательные значения)",)
 
 # 2. Взятие лага
 class LagTransformation(Transformation):
     type: Literal["lag"] = "lag"
-    lag_order: int = Field(..., gt=0)
+    lag_order: int = Field(title="Периоды сдвига для вычисления лага (допустимы отрицательные значения)",)
 
 # 3. Логарифмирование
 class LogTransformation(Transformation):
@@ -23,32 +23,38 @@ class LogTransformation(Transformation):
 # 4. Потенцирование
 class PowTransformation(Transformation):
     type: Literal["pow"] = "pow"
-    pow_order: float = Field(..., gt=0)
+    pow_order: float = Field(..., gt=0, title="Показатель степени")
 
 # 5. Нормализация
 class NormalizeTransformation(Transformation):
     type: Literal["normalize"] = "normalize"
-    method: Literal["standard", "minmax"]
+    method: Literal["standard", "minmax"] = Field(title="Метод, по которому будет проводиться нормализация")
 
 # 6. Экспоненциальное сглаживание (без параметров)
 class ExpSmoothTransformation(Transformation):
     type: Literal["exp_smooth"] = "exp_smooth"
-    span: int = Field(..., gt=0)
+    span: int = Field(gt=0, title="Период окна экспоненциального сглаживания",)
 
 # 7. Преобразование Бокса-Кокса
 class BoxCoxTransformation(Transformation):
     type: Literal["boxcox"] = "boxcox"
-    param: float = Field(...)
+    param: float = Field(title="Параметр λ преобразования Бокса-Кокса",)
 
 # 8. Заполнение пропусков
 class FillMissingTransformation(Transformation):
     type: Literal["fillna"] = "fillna"
-    method: Literal["last", "backward", "mean", "median", "mode"]
+    method: Literal["last", "backward", "mean", "median", "mode"] = Field(
+        title="Метод заполнения пропусков"
+    )
 
 # 9. Скользящее среднее
 class MovingAverageTransformation(Transformation):
     type: Literal["moving_avg"] = "moving_avg"
-    window: int = Field(..., gt=0)
+    window: int = Field(
+        ...,
+        gt=0,
+        title="Размер окна скользящего среднего",
+    )
 
 TransformationUnion = Annotated[
     Union[
@@ -65,7 +71,10 @@ TransformationUnion = Annotated[
     Field(discriminator="type")
 ]
 
-# Модель запроса
+
 class PreprocessingRequest(BaseModel):
-    ts: Timeseries
-    transformations: List[TransformationUnion]  # Список преобразований
+    ts: Timeseries = Field(
+        default=Timeseries(name="Временной ряд для преобразования"),
+        title="Временной ряд для преобразования"
+    )
+    transformations: List[TransformationUnion] = Field(title="Список преобразований")
