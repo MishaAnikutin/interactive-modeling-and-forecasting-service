7a17ed5aaae42c21918fe1f9f294d4e26ff3c96d
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-23 16:20:53 +0300

    обновил доку для сущности Metric

--- app/src/core/domain/metrics/metric.py
+++ app/src/core/domain/metrics/metric.py
@@ -1,12 +1,12 @@
 from typing import Optional
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, Field
 
 from src.shared.utils import validate_float_param
 
 
 class Metric(BaseModel):
-    type: str
+    type: str = Field(default="MAPE", description="Metric type")
     value: Optional[float]
 
     @model_validator(mode="after")
