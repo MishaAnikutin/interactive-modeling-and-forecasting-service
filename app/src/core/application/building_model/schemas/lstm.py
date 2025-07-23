5f7561bcf74ddf3e10918d603cb6ce1542102557
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-23 17:06:59 +0300

    добавил несколько названий для LSTM params

--- app/src/core/application/building_model/schemas/lstm.py
+++ app/src/core/application/building_model/schemas/lstm.py
@@ -8,7 +8,12 @@ from src.core.domain import Timeseries, FitParams, Forecasts, ModelMetrics
 
 
 class LstmParams(BaseModel):
-    input_size: int = Field(default=-1, ge=-1, le=5000, description='maximum sequence length for truncated train')
+    input_size: int = Field(
+        default=-1,
+        ge=-1,
+        le=5000,
+        description='Maximum sequence length for truncated train',
+    )
     inference_input_size: Optional[int]= Field(
         default=None,
         ge=1,
@@ -31,29 +36,64 @@ class LstmParams(BaseModel):
         default=200,
         gt=1,
         le=5000,
-        description="units for the LSTM's hidden state size."
+        description="Units for the LSTM's hidden state size."
     )
     encoder_dropout: float = Field(
         default=0.0,
         ge=0.0,
         le=1.0,
-        description='dropout regularization applied to LSTM outputs.'
+        description='Dropout regularization applied to LSTM outputs.'
     )
     decoder_hidden_size: int = Field(
         default=128,
         ge=0,
         le=5000,
-        description="size of hidden layer for the MLP decoder.size of hidden layer for the MLP decoder."
+        description="Size of hidden layer for the MLP decoder.size of hidden layer for the MLP decoder."
     )
-    decoder_layers: int = Field(default=2, ge=0, le=100, description='number of layers for the MLP decoder.')
-    recurrent: bool = Field(default=False, description='whether to exclude the target variable from the input.')
-    loss: LossEnum = Field(default=LossEnum.MAE,)
-    valid_loss: Optional[LossEnum] = Field(default=None,)
-    max_steps: int = Field(default=100, ge=1, description="Максимум итераций обучения", le=5000)
-    learning_rate: float = Field(default=1e-3, gt=0.0, description="Шаг обучения")
-    early_stop_patience_steps: int = Field(default=-1, ge=-1, description="Patience для early-stopping", le=5000)
-    val_check_steps: int = Field(default=50, ge=0, description="Проверка валидации каждые n шагов", le=5000)
-    scaler_type: ScalerType = Field(default=ScalerType.Robust, description="Тип скейлера")
+    decoder_layers: int = Field(
+        default=2,
+        ge=0,
+        le=100,
+        description='number of layers for the MLP decoder.'
+    )
+    recurrent: bool = Field(
+        default=False,
+        description='whether to exclude the target variable from the input.'
+    )
+    loss: LossEnum = Field(
+        default=LossEnum.MAE,
+        title="Название функции ошибки, используемой при обучении"
+    )
+    valid_loss: Optional[LossEnum] = Field(
+        default=None,
+        title="Название функции ошибки, используемой при валидации"
+    )
+    max_steps: int = Field(
+        default=100,
+        ge=1,
+        description="Максимум итераций обучения",
+        le=5000
+    )
+    learning_rate: float = Field(
+        default=1e-3,
+        gt=0.0,
+        description="Шаг обучения"
+    )
+    early_stop_patience_steps: int = Field(
+        default=-1,
+        ge=-1,
+        description="Patience для early-stopping", le=5000
+    )
+    val_check_steps: int = Field(
+        default=50,
+        ge=0,
+        description="Проверка валидации каждые n шагов",
+        le=5000
+    )
+    scaler_type: ScalerType = Field(
+        default=ScalerType.Robust,
+        description="Тип скейлера"
+    )
 
     @model_validator(mode='after')
     def validate_hidden_size(self):
@@ -77,14 +117,20 @@ class LstmParams(BaseModel):
 
 
 class LstmFitRequest(BaseModel):
-    dependent_variables: Timeseries
-    explanatory_variables: Optional[List[Timeseries]]
-    hyperparameters: LstmParams
-    fit_params: FitParams
+    dependent_variables: Timeseries = Field(
+        default=Timeseries(name="Зависимая переменная"),
+        title="Зависимая переменная"
+    )
+    explanatory_variables: Optional[List[Timeseries]] = Field(
+        default=[Timeseries(name="Объясняющая переменная"), ],
+        title="Список объясняющих переменных"
+    )
+    hyperparameters: LstmParams = Field(title="Параметры модели LSTM")
+    fit_params: FitParams = Field(title="Общие параметры обучения")
 
 
 class LstmFitResult(BaseModel):
-    forecasts: Forecasts          # прогнозы
-    model_metrics: ModelMetrics   # рассчитанные метрики
-    weight_path: str              # путь к сохранённым весам
-    model_id: str                 # идентификатор модели
+    forecasts: Forecasts = Field(title="Прогнозы")
+    model_metrics: ModelMetrics = Field(title="Метрики модели")
+    weight_path: str = Field(default="example.pth", title="Путь до весов модели")
+    model_id: str = Field(default="example", title="Идентификатор модели")
