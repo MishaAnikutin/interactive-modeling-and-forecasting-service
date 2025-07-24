4cb4a77ce96674e4c776921db3cd96501d173e90
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-25 02:27:31 +0300

    обновил доку кпсс (о да)

--- app/src/core/application/preliminary_diagnosis/schemas/kpss.py
+++ app/src/core/application/preliminary_diagnosis/schemas/kpss.py
@@ -1,6 +1,7 @@
 from pydantic import Field, model_validator
 from enum import Enum
 
+from src.core.application.preliminary_diagnosis.errors.kpss import InvalidLagsError
 from src.core.application.preliminary_diagnosis.schemas.common import StatTestParams
 
 
@@ -21,18 +22,20 @@ class KpssParams(StatTestParams):
     nlags: NlagsEnum | int = Field(
         default=NlagsEnum.auto,
         title="Число лагов",
-        description="Indicates the number of lags to be used. "
-                    "If “auto” (default), lags is calculated using "
-                    "the data-dependent method of Hobijn et al. (1998). "
-                    "See also Andrews (1991), Newey & West (1994), "
-                    "and Schwert (1989). "
-                    "If set to “legacy”, uses int(12 * (n / 100)**(1 / 4)) , "
-                    "as outlined in Schwert (1989)."
+        description=(
+            "Параметр указывает количество лагов, которое будет использовано: "
+            "1) Если установлено значение `auto` (по умолчанию), количество лагов рассчитывается автоматически "
+            "с использованием метода, зависящего от данных, предложенного Хобайном и др. (1998). "
+            "Дополнительные ссылки: Эндрюс (1991), Ньюи и Вест (1994), Шверт (1989). "
+            "2) Если установлено значение `legacy`, используется формула `int(12 * (n / 100)**(1 / 4))`, "
+            "описанная в Шверте (1989), где `n` — количество наблюдений. "
+            "3) Если число, то должно быть меньше числа наблюдений и больше или равно 0"
+        )
     )
 
     @model_validator(mode='after')
     def validate_nlags(self):
         if type(self.nlags) != NlagsEnum:
             if self.nlags >= len(self.ts.values) or self.nlags < 0:
-                raise ValueError("Число лагов должно быть меньше числа наблюдений и больше или равно 0")
+                raise ValueError(InvalidLagsError().detail)
         return self
