e612c4ad75923412aaf177dd329fc996612fe85f
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-25 03:41:05 +0300

    Update fit_params.py

--- app/src/core/domain/model/fit_params.py
+++ app/src/core/domain/model/fit_params.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field, model_validator
 class FitParams(BaseModel):
     val_boundary: date = Field(default=date(2030, 11, 30), title="Граница валидационной выборки")
     train_boundary: date = Field(default=date(2029, 5, 31), title="Граница обучающей выборки")
-    forecast_horizon: int = Field(default=12, title="Горизонт прогноза")
+    forecast_horizon: int = Field(default=12, title="Горизонт прогноза", ge=0, le=1000)
 
     @model_validator(mode='after')
     def validate_train_val(self):
