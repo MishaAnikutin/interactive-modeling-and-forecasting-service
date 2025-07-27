5c3868307fe396fd126ce9901955dcd5a5748238
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-27 11:46:19 +0300

    добавил фикстуру на прогноз с экзогенными

--- app/tests/test_api/test_model_diagnosis/test_resid_analysis/fixtures.py
+++ app/tests/test_api/test_model_diagnosis/test_resid_analysis/fixtures.py
@@ -7,7 +7,15 @@ import json
 def forecasts_lstm_base(client):
     json_path = Path("/Users/oleg/projects/interactive-modeling-and-forecasting-service/"
                      "app/tests/data/month/forecasts_lstm_base.json")
-    # Читаем файл и загружаем содержимое в словарь
+    with json_path.open("r", encoding="utf-8") as fp:
+        received_data: dict = json.load(fp)
+
+    return received_data
+
+@fixture(scope="function")
+def forecasts_lstm_exog(client):
+    json_path = Path("/Users/oleg/projects/interactive-modeling-and-forecasting-service/"
+                     "app/tests/data/month/forecasts_lstm_exog.json")
     with json_path.open("r", encoding="utf-8") as fp:
         received_data: dict = json.load(fp)
 
