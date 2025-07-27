e72d5965136fbb38d9bbad467309fc3cb86856dc
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-27 13:17:03 +0300

    вынес общий код в common

--- app/src/core/application/model_diagnosis/schemas/breusch_godfrey.py
+++ app/src/core/application/model_diagnosis/schemas/breusch_godfrey.py
@@ -1,9 +1,8 @@
-from typing import Optional, List
+from typing import Optional
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field
 
 from src.core.application.model_diagnosis.schemas.common import ResidAnalysisData
-from src.shared.utils import validate_float_param
 
 
 class BreuschGodfreyRequest(BaseModel):
@@ -15,30 +14,3 @@ class BreuschGodfreyRequest(BaseModel):
         ge=0, le=10000,
         title="Число лагов"
     )
-
-
-class BreuschGodfreyResult(BaseModel):
-    lmval: Optional[float] = Field(
-        title="LM-статистика",
-        description="Тестовая статистика множителя Лагранжа, используемая для проверки ограничений в модели.",
-    )
-    lmpval: Optional[float] = Field(
-        title="P-значение LM",
-        description="P-значение теста множителя Лагранжа, показывающее вероятность ошибки при отклонении нулевой гипотезы.",
-    )
-    fval: Optional[float] = Field(
-        title="F-статистика",
-        description="F-статистика теста, альтернативная версия теста множителя Лагранжа, основанная на F-тесте для проверки ограничений параметров.",
-    )
-    fpval: Optional[float] = Field(
-        title="P-значение F",
-        description="P-значение F-теста, указывающее на значимость ограничений в модели.",
-    )
-
-    @model_validator(mode="after")
-    def validate_floats(self):
-        self.lmval = validate_float_param(self.lmval)
-        self.lmpval = validate_float_param(self.lmpval)
-        self.fval = validate_float_param(self.fval)
-        self.fpval = validate_float_param(self.fpval)
-        return self
--- app/src/core/application/model_diagnosis/schemas/common.py
+++ app/src/core/application/model_diagnosis/schemas/common.py
@@ -41,3 +41,29 @@ class StatTestResult(BaseModel):
         self.p_value = validate_float_param(self.p_value)
         self.stat_value = validate_float_param(self.stat_value)
         return self
+
+class DiagnosticsResult(BaseModel):
+    lmval: Optional[float] = Field(
+        title="LM-статистика",
+        description="Тестовая статистика множителя Лагранжа, используемая для проверки ограничений в модели.",
+    )
+    lmpval: Optional[float] = Field(
+        title="P-значение LM",
+        description="P-значение теста множителя Лагранжа, показывающее вероятность ошибки при отклонении нулевой гипотезы.",
+    )
+    fval: Optional[float] = Field(
+        title="F-статистика",
+        description="F-статистика теста, альтернативная версия теста множителя Лагранжа, основанная на F-тесте для проверки ограничений параметров.",
+    )
+    fpval: Optional[float] = Field(
+        title="P-значение F",
+        description="P-значение F-теста, указывающее на значимость ограничений в модели.",
+    )
+
+    @model_validator(mode="after")
+    def validate_floats(self):
+        self.lmval = validate_float_param(self.lmval)
+        self.lmpval = validate_float_param(self.lmpval)
+        self.fval = validate_float_param(self.fval)
+        self.fpval = validate_float_param(self.fpval)
+        return self
