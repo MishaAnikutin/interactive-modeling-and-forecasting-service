4df731f4ed8630c0990d9ade249123c597448165
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-07-27 11:45:48 +0300

    Update arch.py

--- app/src/core/application/model_diagnosis/schemas/arch.py
+++ app/src/core/application/model_diagnosis/schemas/arch.py
@@ -3,6 +3,7 @@ from typing import Optional, Literal
 from pydantic import BaseModel, Field, model_validator
 
 from src.core.application.model_diagnosis.schemas.common import ResidAnalysisData, StatTestResult
+from src.shared.utils import validate_float_param
 
 
 class ArchRequest(BaseModel):
@@ -51,11 +52,11 @@ class ArchRequest(BaseModel):
 
 class ArchResult(BaseModel):
     lmval: Optional[float] = Field(
-        title="Множитель Лагранжа",
+        title="LM-статистика",
         description="Тестовая статистика множителя Лагранжа, используемая для проверки ограничений в модели.",
     )
     lmpval: Optional[float] = Field(
-        title="P-значение Лагранжа",
+        title="P-значение LM",
         description="P-значение теста множителя Лагранжа, показывающее вероятность ошибки при отклонении нулевой гипотезы.",
     )
     fval: Optional[float] = Field(
@@ -66,3 +67,11 @@ class ArchResult(BaseModel):
         title="P-значение F",
         description="P-значение F-теста, указывающее на значимость ограничений в модели.",
     )
+
+    @model_validator(mode="after")
+    def validate_floats(self):
+        self.lmval = validate_float_param(self.lmval)
+        self.lmpval = validate_float_param(self.lmpval)
+        self.fval = validate_float_param(self.fval)
+        self.fpval = validate_float_param(self.fpval)
+        return self
