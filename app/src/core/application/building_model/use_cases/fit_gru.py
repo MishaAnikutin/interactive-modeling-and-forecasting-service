f5a19551aae42f67b61ede94aa4361de19b85c17
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-08-12 14:47:14 +0300

    Update fit_gru.py

--- app/src/core/application/building_model/use_cases/fit_gru.py
+++ app/src/core/application/building_model/use_cases/fit_gru.py
@@ -1,4 +1,5 @@
-from src.core.application.building_model.schemas.gru import GruFitRequest, GruFitResponse
+from src.core.application.building_model.schemas.gru import GruFitRequest, GruFitResult
+from src.infrastructure.adapters.archiver import ModelArchiver
 from src.infrastructure.adapters.modeling.gru import GruAdapter
 from src.infrastructure.adapters.serializer import ModelSerializer
 from src.infrastructure.adapters.timeseries import TimeseriesAlignment, PandasTimeseriesAdapter
@@ -10,14 +11,16 @@ class FitGruUC:
         model_adapter: GruAdapter,
         ts_aligner: TimeseriesAlignment,
         ts_adapter: PandasTimeseriesAdapter,
-        model_serializer: ModelSerializer,
+        archiver: ModelArchiver,
+        serializer: ModelSerializer,
     ):
         self._ts_adapter = ts_adapter
         self._ts_aligner = ts_aligner
         self._model_adapter = model_adapter
-        self._model_serializer = model_serializer
+        self._serializer = serializer
+        self._archiver = archiver
 
-    def execute(self, request: GruFitRequest) -> GruFitResponse:
+    def execute(self, request: GruFitRequest) -> bytes:
         target, exog_df = self._ts_aligner.align(request.model_data)
 
         model_result, model_weight = self._model_adapter.fit(
@@ -28,9 +31,9 @@ class FitGruUC:
             data_frequency=request.model_data.dependent_variables.data_frequency
         )
 
-        serialized_model_weight: str = self._model_serializer.serialize(model_weight)
+        data_dict: dict = model_result.model_dump()
+        model_bytes: bytes = self._serializer.serialize(model_weight)
 
-        return GruFitResponse(
-            fit_result=model_result,
-            serialized_model_weight=serialized_model_weight,
-        )
+        archive: bytes = self._archiver.execute(data_dict=data_dict, model_bytes=model_bytes)
+
+        return archive
