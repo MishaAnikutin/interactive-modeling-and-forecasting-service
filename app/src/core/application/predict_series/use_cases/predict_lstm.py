790d51be26df1ce891add768354faab956306ec9
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-08-13 12:09:48 +0300

    сделал UC для нейро прогнозов

--- app/src/core/application/predict_series/use_cases/predict_lstm.py
+++ app/src/core/application/predict_series/use_cases/predict_lstm.py
@@ -1,14 +1,14 @@
 from src.core.application.predict_series.use_cases.neural_predict import BaseNeuralPredict
-from src.infrastructure.adapters.predicting.gru import PredictGruAdapter
+from src.infrastructure.adapters.predicting.lstm import PredictLstmAdapter
 from src.infrastructure.adapters.timeseries import TimeseriesAlignment, PandasTimeseriesAdapter
 
 
-class PredictGruUC(BaseNeuralPredict):
+class PredictLstmUC(BaseNeuralPredict):
     def __init__(
         self,
         ts_aligner: TimeseriesAlignment,
         ts_adapter: PandasTimeseriesAdapter,
-        predict_adapter: PredictGruAdapter,
+        predict_adapter: PredictLstmAdapter,
     ):
         super().__init__(ts_adapter=ts_adapter, ts_aligner=ts_aligner)
         self._predict_adapter = predict_adapter
--- app/src/core/application/predict_series/use_cases/predict_nhits.py
+++ app/src/core/application/predict_series/use_cases/predict_nhits.py
@@ -1,14 +1,15 @@
 from src.core.application.predict_series.use_cases.neural_predict import BaseNeuralPredict
-from src.infrastructure.adapters.predicting.gru import PredictGruAdapter
+from src.infrastructure.adapters.predicting.nhits import PredictNhitsAdapter
 from src.infrastructure.adapters.timeseries import TimeseriesAlignment, PandasTimeseriesAdapter
 
 
-class PredictGruUC(BaseNeuralPredict):
+class PredictNhitsUC(BaseNeuralPredict):
     def __init__(
         self,
         ts_aligner: TimeseriesAlignment,
         ts_adapter: PandasTimeseriesAdapter,
-        predict_adapter: PredictGruAdapter,
+        predict_adapter: PredictNhitsAdapter,
     ):
         super().__init__(ts_adapter=ts_adapter, ts_aligner=ts_aligner)
         self._predict_adapter = predict_adapter
+
