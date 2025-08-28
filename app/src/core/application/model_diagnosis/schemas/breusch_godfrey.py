0394849cbdf39fe91519bfbd93e96badadb20275
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-08-29 00:07:01 +0300

    рефакторинг схем автокор и анализа остатков

--- app/src/core/application/model_diagnosis/schemas/breusch_godfrey.py
+++ app/src/core/application/model_diagnosis/schemas/breusch_godfrey.py
@@ -1,43 +1,11 @@
 from typing import Optional, List
 from pydantic import BaseModel, Field
 
-from src.core.domain import Timeseries, Forecasts
+from src.core.domain import Timeseries, Forecasts, ForecastAnalysis
 from src.core.domain.timeseries.timeseries import gen_values, gen_dates
 
 
-class BreuschGodfreyData(BaseModel):
-    forecasts: Forecasts = Field(
-        default=Forecasts(
-            train_predict=Timeseries(
-                values=gen_values(110)[:70],
-                dates=gen_dates(110)[:70],
-                name="Прогноз на обучающей выборке"
-            ),
-            validation_predict=Timeseries(
-                values=gen_values(110)[70:90],
-                dates=gen_dates(110)[70:90],
-                name="Прогноз на валидационной выборке"
-            ),
-            test_predict=Timeseries(
-                values=gen_values(110)[90:100],
-                dates=gen_dates(110)[90:100],
-                name="Прогноз на тестовой выборке"
-            ),
-            forecast=Timeseries(
-                values=gen_values(110)[100:],
-                dates=gen_dates(110)[100:],
-                name="Прогноз на вне выборки"
-            ),
-        ),
-        title="Прогнозы",
-        description="Даты объединенного прогноза должны совпадать с датами исторических данных."
-    )
-
-    target: Timeseries = Field(
-        title="Исходные данные",
-        default=Timeseries(name="Исходные данные")
-    )
-
+class BreuschGodfreyData(ForecastAnalysis):
     exog: Optional[List[Timeseries]] = Field(
         default=None,
         title="Экзогенные переменные (опционально)"
--- app/src/core/application/model_diagnosis/schemas/common.py
+++ app/src/core/application/model_diagnosis/schemas/common.py
@@ -7,41 +7,6 @@ from src.core.domain.timeseries.timeseries import gen_dates, gen_values
 from src.shared.utils import validate_float_param
 
 
-class ResidAnalysisData(BaseModel):
-    """Прогнозы и исходные данные"""
-    forecasts: Forecasts = Field(
-        default=Forecasts(
-            train_predict=Timeseries(
-                values=gen_values(110)[:70],
-                dates=gen_dates(110)[:70],
-                name="Прогноз на обучающей выборке"
-            ),
-            validation_predict=Timeseries(
-                values=gen_values(110)[70:90],
-                dates=gen_dates(110)[70:90],
-                name="Прогноз на валидационной выборке"
-            ),
-            test_predict=Timeseries(
-                values=gen_values(110)[90:100],
-                dates=gen_dates(110)[90:100],
-                name="Прогноз на тестовой выборке"
-            ),
-            forecast=Timeseries(
-                values=gen_values(110)[100:],
-                dates=gen_dates(110)[100:],
-                name="Прогноз на вне выборки"
-            ),
-        ),
-        title="Прогнозы",
-        description="Даты объединенного прогноза должны совпадать с датами исторических данных."
-    )
-
-    target: Timeseries = Field(
-        title="Исходные данные",
-        default=Timeseries(name="Исходные данные")
-    )
-
-
 class StatTestResult(BaseModel):
     """Результаты статистического теста"""
     p_value: Optional[float] = Field(
