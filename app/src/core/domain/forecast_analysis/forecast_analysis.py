3b97c8d375cb5283e69f97b15b65d8162acc3d0e
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-08-29 00:30:28 +0300

    обновил дефолт значения

--- app/src/core/domain/forecast_analysis/forecast_analysis.py
+++ app/src/core/domain/forecast_analysis/forecast_analysis.py
@@ -1,34 +1,11 @@
 from pydantic import Field, BaseModel
 
 from .. import Forecasts, Timeseries
-from ..timeseries.timeseries import gen_dates, gen_values
 
 
 class ForecastAnalysis(BaseModel):
     """Прогнозы и исходные данные"""
     forecasts: Forecasts = Field(
-        default=Forecasts(
-            train_predict=Timeseries(
-                values=gen_values(110)[:70],
-                dates=gen_dates(110)[:70],
-                name="Прогноз на обучающей выборке"
-            ),
-            validation_predict=Timeseries(
-                values=gen_values(110)[70:90],
-                dates=gen_dates(110)[70:90],
-                name="Прогноз на валидационной выборке"
-            ),
-            test_predict=Timeseries(
-                values=gen_values(110)[90:100],
-                dates=gen_dates(110)[90:100],
-                name="Прогноз на тестовой выборке"
-            ),
-            forecast=Timeseries(
-                values=gen_values(110)[100:],
-                dates=gen_dates(110)[100:],
-                name="Прогноз на вне выборки"
-            ),
-        ),
         title="Прогнозы",
         description="Даты объединенного прогноза должны совпадать с датами исторических данных."
     )
--- app/src/core/domain/model/forecasts.py
+++ app/src/core/domain/model/forecasts.py
@@ -2,24 +2,40 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-from src.core.domain.timeseries.timeseries import Timeseries
+from src.core.domain.timeseries.timeseries import Timeseries, gen_values, gen_dates
 
 
 class Forecasts(BaseModel):
     train_predict: Timeseries = Field(
-        default=Timeseries(name="Прогноз на обучающей выборке"),
+        default=Timeseries(
+            values=gen_values(110)[:70],
+            dates=gen_dates(110)[:70],
+            name="Прогноз на обучающей выборке"
+        ),
         title="Прогноз на обучающей выборке"
     )
     validation_predict: Optional[Timeseries] = Field(
-        default=Timeseries(name="Прогноз на валидационной выборке"),
+        default=Timeseries(
+            values=gen_values(110)[70:90],
+            dates=gen_dates(110)[70:90],
+            name="Прогноз на валидационной выборке"
+        ),
         title="Прогноз на валидационной выборке"
     )
     test_predict: Optional[Timeseries] = Field(
-        default=Timeseries(name="Прогноз на тестовой выборке"),
+        default=Timeseries(
+            values=gen_values(110)[90:100],
+            dates=gen_dates(110)[90:100],
+            name="Прогноз на тестовой выборке"
+        ),
         title="Прогноз на тестовой выборке"
     )
     forecast: Optional[Timeseries] = Field(
-        default=Timeseries(name="Прогноз дальше известных данных"),
+        default=Timeseries(
+            values=gen_values(110)[100:],
+            dates=gen_dates(110)[100:],
+            name="Прогноз на вне выборки"
+        ),
         title="Прогноз дальше известных данных"
     )
 
