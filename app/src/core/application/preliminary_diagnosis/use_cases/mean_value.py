5e97660c1edbbabaefaa6d15fcd3dcf212cfd63e
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-10-24 19:57:55 +0300

    реализовал среднее по массиву

--- app/src/core/application/preliminary_diagnosis/use_cases/mean_value.py
+++ app/src/core/application/preliminary_diagnosis/use_cases/mean_value.py
@@ -1,6 +1,6 @@
 from src.core.application.preliminary_diagnosis.schemas.mean_value import MeanResult, MeanParams
 from src.infrastructure.adapters.timeseries import PandasTimeseriesAdapter
-
+import numpy as np
 
 class MeanUC:
     def __init__(
@@ -10,4 +10,5 @@ class MeanUC:
         self._ts_adapter = ts_adapter
 
     def execute(self, request: MeanParams) -> MeanResult:
-        ...
+        mean = np.mean(request.timeseries.values)
+        return MeanResult(value=round(mean, 4))
