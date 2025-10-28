dd431f22596dc062804be38c879b4a0a2f0e18f9
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-10-28 11:20:23 +0300

    удалил еще кода

--- app/src/api/di/core_provider.py
+++ app/src/api/di/core_provider.py
@@ -14,23 +14,16 @@ from src.core.application.model_diagnosis.use_cases.omnibus import OmnibusUC
 from src.core.application.predict_series.use_cases.predict_gru import PredictGruUC
 from src.core.application.predict_series.use_cases.predict_lstm import PredictLstmUC
 from src.core.application.predict_series.use_cases.predict_nhits import PredictNhitsUC
-from src.core.application.preliminary_diagnosis.use_cases.cv_value import VariationCoeffUC
 from src.core.application.preliminary_diagnosis.use_cases.df_gls import DfGlsUC
 from src.core.application.preliminary_diagnosis.use_cases.dicker_fuller import DickeuFullerUC
 from src.core.application.preliminary_diagnosis.use_cases.kde import KdeUC
 from src.core.application.preliminary_diagnosis.use_cases.kpss import KpssUC
-from src.core.application.preliminary_diagnosis.use_cases.kurtosis import KurtosisUC
-from src.core.application.preliminary_diagnosis.use_cases.mean_value import MeanUC
-from src.core.application.preliminary_diagnosis.use_cases.median_value import MedianUC
-from src.core.application.preliminary_diagnosis.use_cases.mode_value import ModeUC
 from src.core.application.preliminary_diagnosis.use_cases.phillips_perron import PhillipsPerronUC
 from src.core.application.preliminary_diagnosis.use_cases.pp_plot import PPplotUC
 from src.core.application.preliminary_diagnosis.use_cases.qq import QQplotUC
 from src.core.application.preliminary_diagnosis.use_cases.quantiles import QuantilesUC
 from src.core.application.preliminary_diagnosis.use_cases.range_uc import RangeUnitRootUC
-from src.core.application.preliminary_diagnosis.use_cases.skewness import SkewnessUC
 from src.core.application.preliminary_diagnosis.use_cases.statistics import StatisticsUC
-from src.core.application.preliminary_diagnosis.use_cases.var_value import VarianceUC
 from src.core.application.preliminary_diagnosis.use_cases.zivot_andrews import ZivotAndrewsUC
 from src.core.application.preprocessing.preprocessing_uc import PreprocessUC
 from src.core.application.preprocessing.inverse_preprocessing_uc import InversePreprocessUC
@@ -82,14 +75,7 @@ class CoreProvider(Provider):
     acf_pacf_command = provide(AcfAndPacfUC, provides=AcfAndPacfUC)
 
     # Команды для описательной статистики
-    mean_command = provide(MeanUC, provides=MeanUC)
-    median_command = provide(MedianUC, provides=MedianUC)
-    mode_command = provide(ModeUC, provides=ModeUC)
-    variance_command = provide(VarianceUC, provides=VarianceUC)
-    cv_command = provide(VariationCoeffUC, provides=VariationCoeffUC)
     quantiles_command = provide(QuantilesUC, provides=QuantilesUC)
-    skewness_command = provide(SkewnessUC, provides=SkewnessUC)
-    kurtosis_command = provide(KurtosisUC, provides=KurtosisUC)
     statistics_command = provide(StatisticsUC, provides=StatisticsUC)
 
     # Распределения
--- app/src/api/v1/preliminary_diagnosis/descriptive_statistics.py
+++ app/src/api/v1/preliminary_diagnosis/descriptive_statistics.py
@@ -1,69 +1,13 @@
 from fastapi import APIRouter
 from dishka import FromDishka
 from dishka.integrations.fastapi import inject_sync
-
-from src.core.application.preliminary_diagnosis.schemas.cv_value import VariationCoeffParams, VariationCoeffResult
-from src.core.application.preliminary_diagnosis.schemas.kurtosis import KurtosisParams, KurtosisResult
-from src.core.application.preliminary_diagnosis.schemas.mean_value import MeanResult, MeanParams
-from src.core.application.preliminary_diagnosis.schemas.median_value import MedianParams, MedianResult
-from src.core.application.preliminary_diagnosis.schemas.mode_value import ModeParams, ModeResult
 from src.core.application.preliminary_diagnosis.schemas.quantiles import QuantilesResult, QuantilesParams
-from src.core.application.preliminary_diagnosis.schemas.skewness import SkewnessParams, SkewnessResult
 from src.core.application.preliminary_diagnosis.schemas.statistics import StatisticResult, StatisticsEnum
-from src.core.application.preliminary_diagnosis.schemas.var_value import VarianceParams, VarianceResult
-from src.core.application.preliminary_diagnosis.use_cases.cv_value import VariationCoeffUC
-from src.core.application.preliminary_diagnosis.use_cases.kurtosis import KurtosisUC
-from src.core.application.preliminary_diagnosis.use_cases.mean_value import MeanUC
-from src.core.application.preliminary_diagnosis.use_cases.median_value import MedianUC
-from src.core.application.preliminary_diagnosis.use_cases.mode_value import ModeUC
 from src.core.application.preliminary_diagnosis.use_cases.quantiles import QuantilesUC
-from src.core.application.preliminary_diagnosis.use_cases.skewness import SkewnessUC
 from src.core.application.preliminary_diagnosis.use_cases.statistics import StatisticsUC
-from src.core.application.preliminary_diagnosis.use_cases.var_value import VarianceUC
 from src.core.domain import Timeseries
 
 descriptive_statistics_router = APIRouter(prefix="/descriptive_statistics", tags=["Описательная статистика"])
-
-
-@descriptive_statistics_router.post(path="/mean")
-@inject_sync
-def mean_value(
-    request: MeanParams,
-    mean_uc: FromDishka[MeanUC]
-) -> MeanResult:
-    return mean_uc.execute(request=request)
-
-@descriptive_statistics_router.post(path="/median")
-@inject_sync
-def median_value(
-    request: MedianParams,
-    median_uc: FromDishka[MedianUC]
-) -> MedianResult:
-    return median_uc.execute(request=request)
-
-@descriptive_statistics_router.post(path="/mode")
-@inject_sync
-def mode_value(
-    request: ModeParams,
-    mode_uc: FromDishka[ModeUC]
-) -> ModeResult:
-    return mode_uc.execute(request=request)
-
-@descriptive_statistics_router.post(path="/variance")
-@inject_sync
-def variance_value(
-    request: VarianceParams,
-    var_uc: FromDishka[VarianceUC]
-) -> VarianceResult:
-    return var_uc.execute(request=request)
-
-@descriptive_statistics_router.post(path="/coff_of_variation")
-@inject_sync
-def cv_value(
-    request: VariationCoeffParams,
-    cv_uc: FromDishka[VariationCoeffUC]
-) -> VariationCoeffResult:
-    return cv_uc.execute(request=request)
 
 
 @descriptive_statistics_router.post(path="/quantiles")
@@ -74,21 +18,6 @@ def quantiles(
 ) -> QuantilesResult:
     return quant_uc.execute(request=request)
 
-@descriptive_statistics_router.post(path="/skewness")
-@inject_sync
-def skewness(
-    request: SkewnessParams,
-    skewness_uc: FromDishka[SkewnessUC]
-) -> SkewnessResult:
-    return skewness_uc.execute(request=request)
-
-@descriptive_statistics_router.post(path="/kurtosis")
-@inject_sync
-def kurtosis(
-    request: KurtosisParams,
-    kurtosis_uc: FromDishka[KurtosisUC]
-) -> KurtosisResult:
-    return kurtosis_uc.execute(request=request)
 
 @descriptive_statistics_router.post(path="/{statistic}/")
 @inject_sync
