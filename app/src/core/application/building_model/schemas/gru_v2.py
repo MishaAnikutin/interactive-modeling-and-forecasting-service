cc70995bd80c54b81a6bf5799d2f38588c9f16d3
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2025-12-29 16:01:37 +0300

    добавил ошибку с input_size = 0

--- app/src/core/application/building_model/schemas/gru_v2.py
+++ app/src/core/application/building_model/schemas/gru_v2.py
@@ -1,12 +1,22 @@
-from pydantic import Field
+from pydantic import Field, model_validator
 
 from src.core.application.building_model.schemas.gru import GruParams, GruFitRequest
 from src.core.domain import ForecastResult_V2
 
 
 class GruParams_V2(GruParams):
-    input_size: int = Field(ge=1, default=8, title='Размер входного слоя')
+    input_size: int = Field(
+        ge=-1, default=8, title='Размер входного слоя',
+        description='Если поставить -1, то автоматически будет выставлено 3 * output_size, '
+                    'также недопустимо значение равное 0'
+    )
     output_size: int = Field(ge=1, default=2, title='Размер выходного слоя')
+
+    @model_validator(mode='after')
+    def validate_input_size(self):
+        if self.val_size == 0:
+            raise ValueError("input_size должен быть > 0")
+        return self
 
 
 class GruFitRequest_V2(GruFitRequest):
--- app/src/core/application/building_model/schemas/nhits_v2.py
+++ app/src/core/application/building_model/schemas/nhits_v2.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, model_validator
 
 from src.core.application.building_model.schemas import NhitsFitRequest
 from src.core.application.building_model.schemas.nhits import NhitsParams
@@ -6,10 +6,18 @@ from src.core.domain import ForecastResult_V2
 
 
 class NhitsParams_V2(NhitsParams):
-    input_size: int = Field(ge=-1, default=8, title='Размер входного слоя')
-    output_size: int = Field(ge=-1, default=2, title='Размер выходного слоя')
+    input_size: int = Field(
+        ge=-1, default=8, title='Размер входного слоя',
+        description='Если поставить -1, то автоматически будет выставлено 3 * output_size, '
+                    'также недопустимо значение равное 0'
+    )
+    output_size: int = Field(ge=1, default=2, title='Размер выходного слоя')
 
-    # TODO: добавить валидацию этих новых параметров во все схемы (чтоб нельзя было поставить 0)
+    @model_validator(mode='after')
+    def validate_input_size(self):
+        if self.val_size == 0:
+            raise ValueError("input_size должен быть > 0")
+        return self
 
 
 class NhitsFitRequest_V2(NhitsFitRequest):
