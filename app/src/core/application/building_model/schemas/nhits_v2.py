9cc35bc3349449eff3acb7fffcdf28a55b4f92da
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-01-14 16:52:29 +0300

    Update nhits_v2.py

--- app/src/core/application/building_model/schemas/nhits_v2.py
+++ app/src/core/application/building_model/schemas/nhits_v2.py
@@ -7,11 +7,11 @@ from src.core.domain import ForecastResult_V2
 
 class NhitsParams_V2(NhitsParams):
     input_size: int = Field(
-        ge=-1, default=8, title='Размер входного слоя',
+        ge=-1, le=1000, default=8, title='Размер входного слоя',
         description='Если поставить -1, то автоматически будет выставлено 3 * output_size, '
                     'также недопустимо значение равное 0'
     )
-    output_size: int = Field(ge=1, default=2, title='Размер выходного слоя')
+    output_size: int = Field(ge=1, le=1000, default=2, title='Размер выходного слоя')
 
     @model_validator(mode='after')
     def validate_input_size(self):
