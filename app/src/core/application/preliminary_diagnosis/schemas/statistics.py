792b79e4015636d87da958926e7ccb60c3994b37
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-01-20 16:20:30 +0300

    обновил схему

--- app/src/core/application/preliminary_diagnosis/schemas/statistics.py
+++ app/src/core/application/preliminary_diagnosis/schemas/statistics.py
@@ -1,26 +1,16 @@
 from enum import Enum
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 from pydantic import BaseModel, Field, model_validator
 
 from src.core.domain import Timeseries
 
-
-class StatisticsEnum(str, Enum):
-    mean = "mean"
-    median = "median"
-    mode = "mode"
-    variance = "variance"
-    kurtosis = "kurtosis"
-    skewness = "skewness"
-    coefficient_of_variation = "coefficient_of_variation"
-
-
 class StatisticResult(BaseModel):
     """Значение статистики"""
-    value: float | dict[str, float] = Field(
+    value: Optional[float | dict[str, float]] = Field(
         ..., title="Расчетное значение",
-        examples=[3.134, {'Нижний': 123, 'Верхний': 1234}],
+        description="Обычно float, но для специфических случаев может быть dict или не определено",
+        examples=[3.134, {'Нижний': 123, 'Верхний': 1234}, None],
     )
 
     @model_validator(mode='after')
@@ -31,6 +21,7 @@ class StatisticResult(BaseModel):
 
 class RusStatMetricEnum(Enum):
     N_OBS = 'Количество наблюдений'
+    VAR_COEFF = 'Коэффициент вариации'
     MEAN = 'Среднее'
     MEAN_CONF_INT = 'Доверительный интервал для среднего'
     CR_BOUND_MEAN = 'Нижняя граница неравенства Рао–Крамера для среднего (нормальное распределение)'
@@ -56,39 +47,8 @@ class RusStatMetricEnum(Enum):
     ENTROPY = 'Энтропия'
 
 
-class StatMetricEnum(Enum):
-    N_OBS = 'Number of observations'
-    MEAN = 'Mean'
-    MEAN_CONF_INT = 'Mean confidence interval'
-    CR_BOUND_MEAN = 'Rao-Cramer inequality lower bound mean for normal distribution'
-    STD_ERR = 'Standard error'
-    MEDIAN = 'Median'
-    MODE = 'Mode'
-    MEDIAN_WOLSH = 'Median Wolsh'
-    TRIMMED_MEAN = 'Trimmed Mean'
-    STD = 'Standard Deviation'
-    GEOM_MEAN = 'Geometric mean'
-    VAR = 'Variance'
-    VAR_CONF_INT = 'Variance confident interval'
-    CR_BOUND_VAR = 'Rao-Cramer inequality lower bound variance for normal distribution'
-    KURTOSIS = 'Excess kurtosis'
-    SKEW = 'Skewness'
-    MIN = 'Minimum'
-    MAX = 'Maximum'
-    RANGE = 'Range'
-    SUM = 'Sum'
-    Q25 = '25% quantile'
-    Q75 = '75% quantile'
-    LAST_Z = 'Last z-score'
-    ENTROPY = 'Entropy'
-
-    def to_rus(self) -> RusStatMetricEnum:
-        """Преобразует английское название метрики в русское (по идентичным ключам)."""
-        return RusStatMetricEnum[self.name]
-
-
 class StatisticsRequest(BaseModel):
-    metrics: List[StatMetricEnum]
+    metrics: List[RusStatMetricEnum]
     timeseries: Timeseries
 
 class StatisticsResponse(BaseModel):
